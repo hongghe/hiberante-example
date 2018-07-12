81d88f2ad30195628480ccf2b77753e8b2a13109
hongghe <364930855@qq.com>
2018-07-12 19:49:10 +0800

    Initial commit

---
+++ src/main/java/com/example/hiberanteexample/HiberanteExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.hiberanteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberanteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberanteExampleApplication.class, args);
+	}
+}
